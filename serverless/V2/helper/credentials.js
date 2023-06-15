c475f0a7b99beb8b60f9a5500d8c9e411ecce134
prichardsondev <prichardsondev@gmail.com>
2023-06-15 15:20:51 -0400

    update creditials file

--- serverless/V2/helper/credentials.js
+++ serverless/V2/helper/credentials.js
@@ -4,8 +4,8 @@ AWS.config.update({
   region: "us-east-1",
   endpoint: "http://dynamodb.us-east-1.amazonaws.com",
   credentials: new AWS.SharedIniFileCredentials({
-    profile: "sandbox",
+    profile: "yourprofilename",
   }),
 });
 
-module.exports = {AWS, tablename:"youtube"};
+module.exports = {AWS, tablename:"yourtablename"};
